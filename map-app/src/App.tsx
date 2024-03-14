0e4f2952fd6da42111f94a5a7a42dfc943a726fb
richy163 <ritchycal@gmail.com>
2024-03-13 21:09:44 -0400

    added in basic map

--- map-app/src/App.tsx
+++ map-app/src/App.tsx
@@ -1,15 +1,23 @@
 
-import { MapContainer, TileLayer } from 'react-leaflet';
-import './App.css';
+import 'leaflet/dist/leaflet.css';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import "./App.css";
+
 
 function App() {
   const position: [number, number] = [51.505, -0.09]
   const zoom: number = 4
   return (
-    <div className="App">
-      <MapContainer center={position} zoom={zoom} scrollWheelZoom={false}>
-        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" 
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributor' />
+    <div>
+      <MapContainer center={position} zoom={zoom} scrollWheelZoom={false} >
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributor'
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
       </MapContainer>
     </div>
   );
